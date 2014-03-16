af9a5f31c0ba3c2e1a1ef8b7c139e4c50954717d
David Haney <david.a.haney@gmail.com>
2014-03-16 13:10:04 -0400

    Cleaned up FileLogger

--- Dache.Client/Logging/FileLogger.cs
+++ Dache.Client/Logging/FileLogger.cs
@@ -4,16 +4,18 @@ using System.Reflection;
 
 namespace Dache.Core.Logging
 {
-    public class FileLogger : ILogger
+    /// <summary>
+    /// Logs messages and exceptions to the file system. Thread safe.
+    /// </summary>
+    public sealed class FileLogger : ILogger
     {
-        #region ILogger Members
-
-        private readonly string _filePath;
+        // The file path
+        private readonly string _filePath = null;
 
         /// <summary>
         /// The constructor.
         /// </summary>
-        /// <param name="fileName">The name of the log file to write to. Defaults to 'log.txt'</param>
+        /// <param name="fileName">The name of the log file to write to. Defaults to "log.txt" if not specified.</param>
         public FileLogger(string fileName = "log.txt")
         {
             // Sanitize
@@ -23,98 +25,86 @@ namespace Dache.Core.Logging
             }
 
             var currentDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-            _filePath = currentDirectory + "\\" + fileName;
+            _filePath = Path.Combine(currentDirectory, fileName);
         }
 
         /// <summary>
-        /// 
+        /// Logs an information level message.
         /// </summary>
-        /// <param name="title"></param>
-        /// <param name="message"></param>
+        /// <param name="title">The title.</param>
+        /// <param name="message">The message.</param>
         public void Info(string title, string message)
         {
             WriteLine("INFO", title + " : " + message);
         }
 
         /// <summary>
-        /// 
+        /// Logs a warning level message.
         /// </summary>
-        /// <param name="title"></param>
-        /// <param name="message"></param>
+        /// <param name="title">The title.</param>
+        /// <param name="message">The message.</param>
         public void Warn(string title, string message)
         {
             WriteLine("WARN", title + " : " + message);
         }
 
         /// <summary>
-        /// 
+        /// Logs a warning level exception.
         /// </summary>
-        /// <param name="prependedMessage"></param>
-        /// <param name="ex"></param>
+        /// <param name="prependedMessage">The message to prepend to the exception.</param>
+        /// <param name="ex">The exception.</param>
         public void Warn(string prependedMessage, Exception ex)
         {
             WriteLine("WARN", prependedMessage + " : " + ex);
         }
 
         /// <summary>
-        /// 
+        /// Logs a warning level exception.
         /// </summary>
-        /// <param name="ex"></param>
+        /// <param name="ex">The exception.</param>
         public void Warn(Exception ex)
         {
             WriteLine("WARN", ex.ToString());
         }
 
         /// <summary>
-        /// 
+        /// Logs an error level message.
         /// </summary>
-        /// <param name="title"></param>
-        /// <param name="message"></param>
+        /// <param name="title">The title.</param>
+        /// <param name="message">The message.</param>
         public void Error(string title, string message)
         {
             WriteLine("ERROR", title + " : " + message);
         }
 
         /// <summary>
-        /// 
+        /// Logs an error level exception.
         /// </summary>
-        /// <param name="prependedMessage"></param>
-        /// <param name="ex"></param>
+        /// <param name="prependedMessage">The message to prepend to the exception.</param>
+        /// <param name="ex">The exception.</param>
         public void Error(string prependedMessage, Exception ex)
         {
             WriteLine("ERROR", prependedMessage + " : " + ex);
         }
 
         /// <summary>
-        /// 
+        /// Logs an error level exception.
         /// </summary>
-        /// <param name="ex"></param>
+        /// <param name="ex">The exception.</param>
         public void Error(Exception ex)
         {
             WriteLine("ERROR", ex.ToString());
         }
 
         /// <summary>
-        /// 
+        /// Writes a line to the log file.
         /// </summary>
-        /// <param name="level"></param>
-        /// <param name="message"></param>
-        protected virtual void WriteLine(string level, string message)
+        /// <param name="level">The logging level.</param>
+        /// <param name="message">The message.</param>
+        private void WriteLine(string level, string message)
         {
-            WriteRaw(string.Format("{0} {1} {2}{3}", DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss"), level, message, Environment.NewLine));
+            var text = string.Format("{0} {1} {2}{3}", DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss"), level, message, Environment.NewLine);
+            File.AppendAllText(_filePath, text);
         }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="message"></param>
-        protected virtual void WriteRaw(string message)
-        {
-            File.AppendAllText(_filePath, message);
-        }
-
-
-
-        #endregion
     }
 }
